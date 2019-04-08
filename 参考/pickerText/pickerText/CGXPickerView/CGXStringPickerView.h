74ed1e62a5c8420f79883b2a4543ce42aa407eb5
何云海 <yhheh@isoftstone.com>
2019-04-08 16:10:23 +0800

    test

--- 参考/pickerText/pickerText/CGXPickerView/CGXStringPickerView.h
+++ 参考/pickerText/pickerText/CGXPickerView/CGXStringPickerView.h
@@ -17,7 +17,10 @@
 green:((CGFloat)((rgbValue & 0xFF00) >> 8)) / 255.0 \
 blue:((CGFloat)(rgbValue & 0xFF)) / 255.0 alpha:(a)]
 
-#import "CGXPickerViewManager.h"
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@class CGXPickerViewManager;
 
 /**
  *  @param selectValue     选择的行标题文字
@@ -61,4 +64,43 @@ typedef void(^CGXStringResultBlock)(id selectValue,id selectRow);
                 IisAutoSelect:(BOOL)isAutoSelect
                       Manager:(CGXPickerViewManager *)manager
                   ResultBlock:(CGXStringResultBlock)resultBlock;
+@end
+
+@interface CGXPickerViewManager : NSObject
+
+@property (nonatomic , assign) CGFloat kPickerViewH;//选择器高度 默认200
+@property (nonatomic , assign) CGFloat kTopViewH;//按钮高度 默认 50
+
+@property (nonatomic , strong) UIColor *pickerTitleColor;//字体颜色  默认黑色
+@property (nonatomic , assign) CGFloat pickerTitleSize;//字体大小  默认15
+
+@property (nonatomic , strong) UIColor *pickerTitleSelectColor;//字体颜色  默认黑色
+@property (nonatomic , assign) CGFloat pickerTitleSelectSize;//字体大小  默认15
+
+@property (nonatomic , strong) UIColor *lineViewColor;//分割线颜色
+@property (nonatomic , strong) UIColor *titleLabelColor;//中间标题颜色
+@property (nonatomic , strong) UIColor *titleLabelBGColor;//中间标题背景颜色
+@property (nonatomic , assign) CGFloat titleSize;//字体大小
+@property (nonatomic , assign) CGFloat rowHeight; //单元格高度 默认30
+
+@property (nonatomic , strong) UIColor *rightBtnTitleColor;//右侧标题颜色
+@property (nonatomic , strong) UIColor *rightBtnBGColor;//右侧标题背景颜色
+@property (nonatomic , assign) CGFloat rightBtnTitleSize;//字体大小
+@property (nonatomic , strong) NSString *rightBtnTitle;//右侧文字
+@property (nonatomic , assign) CGFloat rightBtnCornerRadius;//右侧圆角
+@property (nonatomic , assign) CGFloat rightBtnBorderWidth;//右侧边框宽
+@property (nonatomic , strong) UIColor *rightBtnborderColor;//右侧边框颜色
+
+
+@property (nonatomic , strong) UIColor *leftBtnTitleColor;//右侧标题颜色
+@property (nonatomic , strong) UIColor *leftBtnBGColor;//右侧标题背景颜色
+@property (nonatomic , assign) CGFloat leftBtnTitleSize;//字体大小
+@property (nonatomic , strong) NSString *leftBtnTitle;//右侧文字
+@property (nonatomic , assign) CGFloat leftBtnCornerRadius;//右侧圆角
+@property (nonatomic , assign) CGFloat leftBtnBorderWidth;//右侧边框宽
+@property (nonatomic , strong) UIColor *leftBtnborderColor;//右侧边框颜色
+
+
+@property (nonatomic , assign) BOOL isHaveLimit; //是否有 “不限”选项  默认没有
+
 @end
